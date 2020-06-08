9b390a6bedfa6a5d02cf9809b1599da6a4aad4b1
Victor Li <victor.li.1995@hotmail.com>
2020-06-07 23:11:46 -0400

    kind of works

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -3,46 +3,13 @@
 import React, {Component} from 'react';
 
 class AccountBalance extends Component {
-// constructor(props){
-//     super(props);
-//     this.state={
-//         accountBalance: this.props.accountBalance }
-// }
-
-// componentWillMount()
-// {
-//     console.log("BEFORE " +this.state.accountBalance );
-//     this.setState({accountBalance: this.props.accountBalance})
-//     console.log("HERE " +this.props.accountBalance );
-// }
-
-
-// componentWillReceiveProps( nextProps)
-// {
-//    // console.log("BEFORE " +this.state.accountBalance );
-//     if(nextProps != this.props)
-//     {
-//     this.setState({accountBalance: nextProps.accountBalance})
-//     console.log("currentProp " +this.props.accountBalance );
-//     console.log("next " + nextProps.accountBalance);
-    
-//     }
-// }
-
-// componentDidMount() {
-//     const storedValue = localStorage.getItem(this.props.accountBalance);
-//     console.log("@@"+storedValue);
-//     console.log("##" +this.props.accountBalance)
-//     if (storedValue) {
-//       this.setState({ accountBalance: storedValue });
-//     }}
 
 
   render() {
       //console.log("balance state " + this.state.accountBalance)
     return (
         <div>
-          Balance: ${this.props.accountBalance}
+          Balance: ${(Math.round(this.props.accountBalance* 100) / 100).toFixed(2)}
         </div>
     );
   }
